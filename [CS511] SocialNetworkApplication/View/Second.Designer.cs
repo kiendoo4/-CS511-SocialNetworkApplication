9ed25ff98d2877760ed67ad2cd19040b01151806
Trung Kien Nguyen <koke36354@gmail.com>
2024-06-12 17:43:13 +0700

    change sth

--- [CS511] SocialNetworkApplication/View/Second.Designer.cs
+++ [CS511] SocialNetworkApplication/View/Second.Designer.cs
@@ -31,13 +31,13 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Second));
             this.userUC = new System.Windows.Forms.Panel();
             this.socialUC = new System.Windows.Forms.Panel();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
             this.label1 = new System.Windows.Forms.Label();
             this.panel2 = new System.Windows.Forms.Panel();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.textBox1 = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.messageUC = new System.Windows.Forms.Panel();
             this.flowLayoutPanel2 = new System.Windows.Forms.FlowLayoutPanel();
-            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
             this.socialUC.SuspendLayout();
             this.panel2.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
@@ -59,13 +59,20 @@
             this.socialUC.Size = new System.Drawing.Size(853, 903);
             this.socialUC.TabIndex = 1;
             // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(853, 903);
+            this.flowLayoutPanel1.TabIndex = 0;
+            // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Paytone One", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(13, 19);
+            this.label1.Font = new System.Drawing.Font("Paytone One", 15F);
+            this.label1.Location = new System.Drawing.Point(271, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(224, 48);
+            this.label1.Size = new System.Drawing.Size(185, 40);
             this.label1.TabIndex = 12;
             this.label1.Text = "Người liên hệ";
             // 
@@ -74,11 +81,20 @@
             this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.panel2.Controls.Add(this.textBox1);
             this.panel2.Controls.Add(this.pictureBox1);
-            this.panel2.Location = new System.Drawing.Point(13, 77);
+            this.panel2.Location = new System.Drawing.Point(13, 43);
             this.panel2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.panel2.Name = "panel2";
             this.panel2.Size = new System.Drawing.Size(443, 40);
             this.panel2.TabIndex = 13;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Quicksand SemiBold", 10F);
+            this.textBox1.Location = new System.Drawing.Point(54, 6);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(384, 28);
+            this.textBox1.TabIndex = 1;
             // 
             // pictureBox1
             // 
@@ -92,15 +108,6 @@
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             // 
-            // textBox1
-            // 
-            this.textBox1.Font = new System.Drawing.Font("Quicksand SemiBold", 10F);
-            this.textBox1.Location = new System.Drawing.Point(54, 6);
-            this.textBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(384, 28);
-            this.textBox1.TabIndex = 1;
-            // 
             // messageUC
             // 
             this.messageUC.Controls.Add(this.flowLayoutPanel2);
@@ -113,17 +120,10 @@
             // 
             // flowLayoutPanel2
             // 
-            this.flowLayoutPanel2.Location = new System.Drawing.Point(0, 138);
+            this.flowLayoutPanel2.Location = new System.Drawing.Point(0, 88);
             this.flowLayoutPanel2.Name = "flowLayoutPanel2";
-            this.flowLayoutPanel2.Size = new System.Drawing.Size(456, 765);
+            this.flowLayoutPanel2.Size = new System.Drawing.Size(456, 815);
             this.flowLayoutPanel2.TabIndex = 14;
-            // 
-            // flowLayoutPanel1
-            // 
-            this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 0);
-            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
-            this.flowLayoutPanel1.Size = new System.Drawing.Size(853, 903);
-            this.flowLayoutPanel1.TabIndex = 0;
             // 
             // Second
             // 
